8575c3aeadac2cc96eff07b595220de2d32711d8
KimSungHo-web <epdlam20@gmail.com>
2024-11-15 17:26:38 +0900

    feat : Enemys

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //플레이어를 추력해야한다 -> 플레이어 위치와 체력을 기반으로 추적 
+    Transform player;
+    //참조해야돨듯? 플레이어 체력
+    EnemyHealth enemyHealth;
+    UnityEngine.AI.NavMeshAgent agent;
+
+    void Awake()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        //참조해야돨듯? 플레이어 체력
+        enemyHealth = GetComponent<EnemyHealth>();
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (enemyHealth.currentHealth > 0/* && 플레이어가 살아있으면*/) 
+        {
+            //경로를 새롭게 쓴다(플레이어의 위치)
+            agent.SetDestination(player.position);
+        }
+        else 
+        {
+            //아님 말구
+            agent.enabled = false;
+        }
     }
 }
