2e8eb3dccbf006ac768ec062e245df3993ed0458
Yggfyhe <alstjqdl1@naver.com>
2024-11-18 09:10:15 +0900

    feat : generic singleton 추가

---
+++ Assets/Scripts/Managers/ItemManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : Singleton<ItemManager>
+{
+    private Dictionary<string, GameObject> prefabDict = new Dictionary<string, GameObject>();
+    private Dictionary<string, Sprite> spriteDict = new Dictionary<string, Sprite>();
+    private Dictionary<string, ScriptableObject> soDict = new Dictionary<string, ScriptableObject>();
+
+
+
+
+
+
+
+}
